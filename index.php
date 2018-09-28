2a8905591d4f6509eea5f7898d9376b30d123d8b
hhy4285 <yi998hh@gmail.com>
2018-09-28 13:39:16 -0400

    indexphp

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+
+  </head>
+
+  <header>
+  </header>
+
+  <body>
+
+      <footer>© Copyright ....</footer>
+  </body>
+
+
+
+</html>
