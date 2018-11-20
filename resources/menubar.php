6310c8281f92df8319e7eb876004ab4269423c47
hhy4285 <yi998hh@gmail.com>
2018-11-20 12:02:46 -0500

    css + menu bar
    Please use a separate file for the css of your own page so we can use a shared css for the things we have in all pages, for example, menu bar. I put menu bar into a php file so we can use that when we have back-end implemented. It's easier to handle doing this so we don't have to change the menu bar parts in all files when we want to make changes on it.

---
+++ resources/menubar.php
@@ -0,0 +1,7 @@
+<?php
+	echo "<ul id="homebar">
+      <li><a href="index.html"> HOME </a></li>
+      <li class="right"><a href="user_profile.html"> MY PROFILE </a></li>
+      <li class="right"><a href="login.html"> LOGIN </a></li>
+    </ul>";
+?>
