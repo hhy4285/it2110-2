3ec0f9b9f0f405fcdc4c9779bc8191f823cd8d46
hhy4285 <yi998hh@gmail.com>
2018-11-25 18:06:02 -0500

    menu bar update + css update
    footer needs help

--- resources/menubar.php
+++ resources/menubar.php
@@ -1,5 +1,5 @@
 <?php
 	echo "<ul id="homebar">
       <li><a href="index.html"> HOME </a></li>
-    ";
+      <div class="right">";
 ?>
--- resources/menubar_in_html.php
+++ resources/menubar_in_html.php
@@ -8,11 +8,12 @@
 <?php
 	include ('resources/menubar.php');
 	if(isset($_SESSION[''])){
-	  print_r("<li class="right"><a href='user_profile.html'>" . $username . "</a>");
-	  print_r("<li class="right"><a href='resources/logout.php'>Log out</a>");
+	  print_r("<li><a href='resources/logout.php'>Log out</a>");
+	  print_r("<li><a href='user_profile.html'>" . $username . "</a>");
 	}
 	else{
-	  print_r("<li class="right"><a href="login.html"> LOGIN </a></li>");
+	  print_r("<li><a href="login.html"> LOGIN </a></li>");
 	}
-	print_r("</ul>")
+	print_r("<div/>
+		</ul>")
 ?>
